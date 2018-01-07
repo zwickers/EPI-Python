410316c58f17998c8e2aea021c195f1d1ce4c0ca
Justin Zwick <zwickyj@gmail.com>
2018-01-07 14:27:37 -0500

    fix time complexity and added problem description

--- Arrays/01_dutch_flag_partitioning.py
+++ Arrays/01_dutch_flag_partitioning.py
@@ -1,3 +1,10 @@
+#
+#   Question: Write a program that takes an array A and an index i,
+#   and rearranges the elements such that all elements less than A[i] (the pivot)
+#   appear first, followed by elements equal to the pivot, followed by elements
+#   greater than the pivot
+#
+
 def dutch_flag_partitioning(A,pivot_index):
     pivot = A[pivot_index]
     less,greater = 0,len(A)-1
@@ -20,5 +27,5 @@ def dutch_flag_partitioning(A,pivot_index):
             greater -= 1
 
 # each iteration decreases the size of unclassified by 1, and the time spent on each
-# iteration is O(1), so the time complexity is O(1)
+# iteration is O(1), so the time complexity is O(n)
 # since this is solved in-place, the space complexity is O(1)
